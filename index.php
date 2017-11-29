632fff4b23c3405426efac48d4479f28730ede39
root <root@localhost.localdomain>
2017-11-29 14:14:32 -0500

    Outro commit

--- index.php
+++ index.php
@@ -1,10 +1,4 @@
 <?php
 echo "<h1>Openshift Workshop o/</h1> ";
-<<<<<<< HEAD
-echo $_SERVER['SERVER_ADDR'];
 echo "#### FIM ####";
-=======
-echo $_SERVER['SERVER_ADDR']
-echo "  :)";
->>>>>>> 497d8ed69884543fa390da315b0aad5f51910ad6
 ?>
